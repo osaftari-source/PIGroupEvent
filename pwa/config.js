3923e189d24ff80be695ca2edeeb5925645eb1c8
osaftari-source <osaftari@gmail.com>
2026-07-26 22:09:06 +0700

    Delete pwa directory

--- pwa/config.js
+++
@@ -1,8 +0,0 @@
-window.APP_CONFIG = {
-  API_URL: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE",
-  API_MODE: "auto",
-  REFRESH_SECONDS: 120,
-  GOOGLE_SHEET_URL: "https://script.google.com/macros/s/AKfycbyv6LbN0vmO5C--NIqQfKEMGzmBMLj8Zfv89JNOQT_lU0YdqodVe7ODrWN4-YoT4F6JPQ/exec",
-  SHOW_EDIT_SHEET_BUTTON: true,
-  SAMPLE_DATA_URL: "./data/sample.json"
-};
