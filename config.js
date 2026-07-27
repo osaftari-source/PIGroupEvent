37317eb904b3ff7c3b538907ac48806b06aa52bf
osaftari-source <osaftari@gmail.com>
2026-07-27 08:11:51 +0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-window.APP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbyv6LbN0vmO5C--NIqQfKEMGzmBMLj8Zfv89JNOQT_lU0YdqodVe7ODrWN4-YoT4F6JPQ/exec",
-  API_MODE: "auto",
-  REFRESH_SECONDS: 120,
-  GOOGLE_SHEET_URL: "https://docs.google.com/spreadsheets/d/1BTAdWvVX5YOFn7IbmAbVnXZ-PX1fm90ySMkyDTCFHZw/edit?usp=sharing",
-  SHOW_EDIT_SHEET_BUTTON: true,
-  SAMPLE_DATA_URL: "./data/sample.json"
-};
